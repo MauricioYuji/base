93b87726829ed4f26f2289b913bd44d7080c6c7b
Yuji <yuji.shima09@gmail.com>
2018-09-17 00:23:20 -0300

    First commit

---
+++ GameAdm/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+import { Observable } from 'rxjs';
+class Game {
+  constructor(public nome) { }
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'GameAdm';
+  public games: AngularFireList<Game[]>;
+  constructor(db: AngularFireDatabase) {
+    this.games = db.list('/Games').valueChanges();
+    console.log("db", this.games);
+  }
+}
