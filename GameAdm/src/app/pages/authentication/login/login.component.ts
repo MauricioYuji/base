1995ab5d7ee87ba8dc73be9fa1afabec31ecc78f
Yuji <yuji.shima09@gmail.com>
2018-11-09 00:04:02 -0200

    layout login

--- GameAdm/src/app/pages/authentication/login/login.component.ts
+++ GameAdm/src/app/pages/authentication/login/login.component.ts
@@ -18,6 +18,8 @@ export class LoginComponent {
 
   }
   login() {
+    console.log("this.email:", this.email);
+    console.log("this.password:", this.password);
     this.authService.login(this.email, this.password);
     this.email = this.password = '';
   }
